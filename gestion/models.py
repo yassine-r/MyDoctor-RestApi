628486afe75b115691a3eee67821051f85237d5a
Yassine Rachidy <92187033+yassine-r@users.noreply.github.com>
2022-06-01 17:34:30 +0000

    Update models.py

--- gestion/models.py
+++ gestion/models.py
@@ -8,7 +8,7 @@ from users.models import Patient, Facility
 class Order(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
-    date = models.CharField(max_length=60,null=True, blank=True)
+    date = models.DateTimeField(max_length=60,null=True, blank=True)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     facility = models.ForeignKey(Facility, on_delete=models.CASCADE)
     Description = models.TextField(max_length=500, null=True, blank=True)
